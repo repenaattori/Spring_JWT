66f77210f284fdd03a442c0e1f2995e4ca576cb5
repenaattori <reimarii@oamk.fi>
2024-04-10 21:10:01 +0300

    Added cross origin configuration.

--- src/main/java/com/security/auth/rest/UserRestApi.java
+++ src/main/java/com/security/auth/rest/UserRestApi.java
@@ -2,23 +2,35 @@ package com.security.auth.rest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.security.auth.service.MySecurityService;
+
 @RestController
+@CrossOrigin(originPatterns = "*", allowCredentials = "true")
+//The other option is to restrict only the React dev origin locally:
+//@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class UserRestApi {
+
+
+    MySecurityService secService;
+
+    public UserRestApi(MySecurityService secService){
+        this.secService = secService;
+    }
     
     @GetMapping("private/userdata")
     public ResponseEntity<String> getPrivateData(@RequestAttribute(name="username") String username){
-
         //We get here only if the MyTokenFilter gets through and the user is validated.
         return new ResponseEntity<String>("Personal data for " + username, HttpStatus.OK);
     }
 
-    // @GetMapping("private/usedata")
+    // @GetMapping("private/userdata")
     // public ResponseEntity<String> getPrivateData(@RequestHeader("Authorization") String bearer){
-
+        
     //     if(bearer.startsWith("Bearer")){
     //         String token = bearer.split(" ")[1];
     //         String username = secService.validateJwt(token);
